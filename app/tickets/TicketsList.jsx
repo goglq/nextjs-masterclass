47a97a0468db91c65d88ad185db5812c7cd996da
Alexey Nazarov <goglqfree@gmail.com>
2024-03-14 22:22:18 +0300

    finale

--- app/tickets/TicketsList.jsx
+++ app/tickets/TicketsList.jsx
@@ -1,11 +1,11 @@
 import Link from 'next/link'
 
 async function getTickets() {
-    await new Promise((resolve) => setTimeout(resolve, 3000))
+    await new Promise((resolve) => setTimeout(resolve, 1000))
 
     const res = await fetch('http://localhost:4000/tickets', {
         next: {
-            revalidate: 30,
+            revalidate: 0,
         },
     })
 
--- app/tickets/page.jsx
+++ app/tickets/page.jsx
@@ -1,17 +1,26 @@
 import { Suspense } from 'react'
+
+import Link from 'next/link'
+
 import TicketsList from './TicketsList'
 import Loading from './loading'
 
 export default function Tickets() {
     return (
         <main>
-            <nav>
+            <nav className="flex justify-between">
                 <div>
                     <h2>Tickets</h2>
                     <p>
                         <small>Currently open tickets.</small>
                     </p>
                 </div>
+                <Link
+                    className="bg-primary text-white px-2 py-1"
+                    href="/tickets/create"
+                >
+                    Create Ticket
+                </Link>
             </nav>
             <Suspense fallback={<Loading />}>
                 <TicketsList />
